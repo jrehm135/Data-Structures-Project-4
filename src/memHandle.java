c645fd50774e47bef94037d3ccbd8e2dfec9aeb4
Josh Rehm <jrehm135@vt.edu>
2019-04-22 16:48:46 -0400

    Added some untested hash code and edited memHandle

--- src/memHandle.java
+++ src/memHandle.java
@@ -10,6 +10,7 @@ public class memHandle {
     private int memLoc;
     private int memLength;
     
+    
     memHandle(int loc, int length){
         this.memLoc = loc;
         this.memLength = length;
--- src/sequenceHash.java
+++ src/sequenceHash.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
 /**
  * 
  */
@@ -6,8 +9,21 @@
  * @author Josh
  *
  */
-public class sequenceHash implements hashTable {
-
+public class sequenceHash<T extends memHandle> implements hashTable<memHandle> {
+    private int[] tableArray;
+    private int tableSize;
+    private RandomAccessFile file;
+    
+    private final int BUCKETSIZE = 32;
+    
+    sequenceHash(int tableSize) throws FileNotFoundException{
+        tableArray = new int[tableSize];
+        this.tableSize = tableSize;
+        
+        file = new RandomAccessFile("hashFile.bin", "rw");
+    }
+    
+    //This implements the 
     @Override
     public long hash(String s, int M) {
         int intLength = s.length() / 4;
@@ -33,14 +49,39 @@ public class sequenceHash implements hashTable {
     }
 
     @Override
-    public void insert(Object e) {
-        // TODO Auto-generated method stub
+    public void insert(String seqID, memHandle handle) {
+        long hashSlot = hash(seqID, tableSize);
+        int bucket = (int) hashSlot / 32;
+        int currSlot = (int) hashSlot % 32;
         
+        //Try first slot to check for availability
+        if(tableArray[(int)hashSlot] == 0 &&
+                tableArray[(int)hashSlot + 1] == 0) {
+            tableArray[(int)hashSlot] = handle.getMemLoc();
+            tableArray[(int)hashSlot + 1] = handle.getMemLength();
+        }
+        //Move to next slot
+        currSlot = (currSlot + 2) % 64;
+        
+        //Iterate through slots until one is found
+        while((bucket * 32) + currSlot != hashSlot) {
+            if(tableArray[(bucket * 32) + currSlot] == 0 &&
+                    tableArray[(bucket * 32) + currSlot + 1] == 0) {
+                
+                tableArray[(bucket * 32) + currSlot] = 
+                        handle.getMemLoc();
+                tableArray[(bucket * 32) + currSlot + 1] = 
+                        handle.getMemLength();
+                break;
+            }
+            
+            //Move to next slot
+            currSlot = (currSlot + 2) % 64;
+        }
     }
 
     @Override
-    public void remove(Object e) {
-        // TODO Auto-generated method stub
+    public void remove(String seqID) {
         
     }
 
