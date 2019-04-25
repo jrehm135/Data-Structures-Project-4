76cd7abe4a65b48c84cf775e7ea5e5dcd3e917ef
Quinton <qemiller@vt.edu>
2019-04-25 19:20:38 -0400

    added file to test insert for the memory manager

--- src/MemoryManTest.java
+++ src/MemoryManTest.java
@@ -1,4 +1,36 @@
+import junit.framework.TestCase;
 
-public class MemoryManTest {
+public class MemoryManTest extends TestCase{
+    public MemoryMan testMan;
+
+
+    public void testInit() {
+        try {
+            testMan = new MemoryMan(1000, "bioFile.bin");
+            assertTrue(testMan.getCurMemSize() == 1000);
+        }
+        catch (Exception IOException) {
+            System.out.println("Could not find memory file.");
+        }
+    }
+    
+    public void testInsert() {
+        try {
+            testMan = new MemoryMan(1000, "bioFile.bin");
+        }
+        catch (Exception IOException) {
+            System.out.println("Could not find memory file.");
+            return;
+        }
+        testMan.insert("AAAAA", "AAAATTTTCCCCGGGGAAAACCCCGGGGTTTTAAAATTTT");//2 + 10 = 12
+        DoublyLinkedList <FreeBlock> testList = testMan.getfreeBlocksList();
+        testList.moveToHead();
+        testList.next();
+        FreeBlock tempBlock = testList.getElement();
+        assertTrue(tempBlock.getPos() == 12);
+        assertTrue(tempBlock.getLength() == 988);
+        
+        
+    }
 
 }
