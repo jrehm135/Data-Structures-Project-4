f3b74e41c37ece91dc5636a5069a6bfac34d0704
Quinton <qemiller@vt.edu>
2019-04-30 02:36:07 -0400

    added search function

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.io.RandomAccessFile;
 
 /**
@@ -9,10 +10,17 @@ import java.io.RandomAccessFile;
  *
  */
 public interface HashTable<T> {
-    //Define the string hash function
+    // Define the string hash function
     public long hash(String s, int M);
-    
+
+
     public boolean insert(String seqID, T[] e);
-    
-    public boolean remove(String seqID, RandomAccessFile file);
+
+
+    public boolean remove(String seqID, RandomAccessFile file)
+        throws IOException;
+
+
+    public boolean search(String seqID, String sequence, RandomAccessFile file)
+        throws IOException;
 }
--- src/SequenceHash.java
+++ src/SequenceHash.java
@@ -14,93 +14,114 @@ public class SequenceHash<T extends MemHandle> implements HashTable<MemHandle> {
     private MemHandle[][] tableArray;
     private int tableSize;
     private RandomAccessFile file;
-    
-    //Open up a hash file and allocate space for MemHandles
-    SequenceHash(int tableSize, String hashFile) throws FileNotFoundException{
-        //Create a matrix with size tablesize and 2 elements per row
+
+
+    // Open up a hash file and allocate space for MemHandles
+    SequenceHash(int tableSize, String hashFile) throws FileNotFoundException {
+        // Create a matrix with size tablesize and 2 elements per row
         tableArray = new MemHandle[tableSize][2];
         this.tableSize = tableSize;
-        
+
         file = new RandomAccessFile(hashFile, "rw");
     }
-    
-    //This implements the sfold hash function
+
+
+    // This implements the sfold hash function
     @Override
     public long hash(String s, int M) {
         int intLength = s.length() / 4;
         long sum = 0;
         for (int j = 0; j < intLength; j++) {
-          char c[] = s.substring(j * 4, (j * 4) + 4).toCharArray();
-          long mult = 1;
-          for (int k = 0; k < c.length; k++) {
-            sum += c[k] * mult;
-            mult *= 256;
-          }
+            char c[] = s.substring(j * 4, (j * 4) + 4).toCharArray();
+            long mult = 1;
+            for (int k = 0; k < c.length; k++) {
+                sum += c[k] * mult;
+                mult *= 256;
+            }
         }
 
         char c[] = s.substring(intLength * 4).toCharArray();
         long mult = 1;
         for (int k = 0; k < c.length; k++) {
-          sum += c[k] * mult;
-          mult *= 256;
+            sum += c[k] * mult;
+            mult *= 256;
         }
 
         sum = (sum * sum) >> 8;
-        return(Math.abs(sum) % M);
+        return (Math.abs(sum) % M);
     }
-    
-    //Insert a memory handle into hashtable
+
+
+    // Insert a memory handle into hashtable
     @Override
     public boolean insert(String seqID, MemHandle[] handles) {
         long hashSlot = hash(seqID, tableSize);
-        int bucket = (int) hashSlot / 32;
-        int currSlot = (int) hashSlot % 32;
-        
-        //Try first slot to check for availability
-        //We only need to check the first value since the values are related
-        if(tableArray[(int)hashSlot][0] == null) {
+        int bucket = (int)hashSlot / 32;
+        int currSlot = (int)hashSlot % 32;
+
+        // Try first slot to check for availability
+        // We only need to check the first value since the values are related
+        if (tableArray[(int)hashSlot][0] == null) {
             tableArray[(int)hashSlot] = handles;
             return true;
         }
-        //Move to next slot
+        // Move to next slot
         currSlot = (currSlot + 1) % 32;
-        
-        //Iterate through slots until one is found
-        while((bucket * 32) + currSlot != hashSlot) {
-            if(tableArray[(bucket * 32) + currSlot][0] == null) {
+
+        // Iterate through slots until one is found
+        while ((bucket * 32) + currSlot != hashSlot) {
+            if (tableArray[(bucket * 32) + currSlot][0] == null) {
                 tableArray[(bucket * 32) + currSlot] = handles;
                 return true;
             }
-            
-            //Move to next slot
+
+            // Move to next slot
             currSlot = (currSlot + 1) % 32;
         }
-        
-        //If we make it here, then the bucket is full, reject insert
+
+        // If we make it here, then the bucket is full, reject insert
         return false;
     }
 
+
     @Override
-    public boolean remove(String seqID, RandomAccessFile file) throws IOException {
+    public boolean remove(String seqID, RandomAccessFile file)
+        throws IOException {
         long hashSlot = hash(seqID, tableSize);
-        int bucket = (int) hashSlot / 32;
-        int currSlot = (int) hashSlot % 32;
-        
-        //Try first slot to check for availability
-        //We only need to check the first value since the values are related
-        if(tableArray[(int)hashSlot][0] == null) {
+        int bucket = (int)hashSlot / 32;
+        int currSlot = (int)hashSlot % 32;
+
+        // Try first slot to check for availability
+        // We only need to check the first value since the values are related
+        if (tableArray[(int)hashSlot][0] == null) {
             return false;
         }
         else {
             MemHandle idHandle = tableArray[(int)hashSlot][0];
             int offset = idHandle.getMemLoc();
-            int length = idHandle.getMemLength()();
+            int length = idHandle.getMemLength();
             file.seek(offset);
-            file.read(b, 0, );;
+            file.read(b, 0);
         }
-        //Move to next slot
+        // Move to next slot
         currSlot = (currSlot + 1) % 32;
         return true;
+    }
+
+
+    @Override
+    public boolean search(String seqID, String sequence, RandomAccessFile file)
+        throws IOException {
+        long hashSlot = hash(seqID, tableSize);
+        int bucket = (int)hashSlot /32;
+        int currSlot = (int)hashSlot % 32;
+        
+        //look at first slot
+        if(tableArray[(int)hashSlot][0] == null)
+        {
+            return false;
+        }
+        return false;
     }
 
 }
